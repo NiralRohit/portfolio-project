6817baa2f081329f030a898a4ff24aa42f22dd00
NiralRohit <23it110@charusat.edu.in>
2025-09-13 23:07:45 +0530

    Updated About section and modified PR3.html and p3.js

--- p3.js
+++ p3.js
@@ -1,8 +1,6 @@
 // Portfolio Website Script
-
 console.log("Portfolio website script loaded.");
-
 // Example: Display a welcome alert
 window.onload = () => {
-  alert("Welcome to My Portfolio Website 🚀");
+  alert("Welcome to My Portfolio Website ");
 };
