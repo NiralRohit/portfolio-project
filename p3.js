3bc4d3306f185b661c8514c2ab8d7f333711b702
NiralRohit <23it110@charusat.edu.in>
2025-09-13 22:53:49 +0530

    Initial commit: Portfolio Website

---
+++ p3.js
@@ -0,0 +1,8 @@
+// Portfolio Website Script
+
+console.log("Portfolio website script loaded.");
+
+// Example: Display a welcome alert
+window.onload = () => {
+  alert("Welcome to My Portfolio Website 🚀");
+};
